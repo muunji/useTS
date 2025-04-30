a360c171482e08643c53a0862b5b4aa4df305861
김민지 <muunji@github.com>
2025-04-30 10:06:49 +0900

    객체 작성

--- test.ts
+++ test.ts
@@ -1,2 +1,12 @@
 let foodList : string[] = ['빵',"떡","밥"]
 let lotto :number[]= [1,2,3]
+
+let person:{
+  name : string,
+  age: number,
+  hobby : string[]
+}={
+  name:'mj',
+  age:20,
+  hobby:['빵','커피']
+}
