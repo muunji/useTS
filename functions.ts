1a0d923ead9f0c7f9104f705170b65b6328f9f91
김민지 <twoji3019@naver.com>
2025-05-01 09:29:50 +0900

    함수 작성해봄

---
+++ functions.ts
@@ -0,0 +1,14 @@
+// 매개변수와 반환값 같은 함수
+function add (a:number, b:number):number{
+  return a+b
+}
+
+//매개변수와 반환값 다른 함수
+function isAdult(age:number):boolean{
+  return age >=20
+}
+
+//string 타입
+function greet(name:string):string{
+  return `Hello, ${name}`
+}
